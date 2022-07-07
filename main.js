5e5ab4cb9bd76533354ff9509e46fc85975387c5
vishwajabakathatla <88708444+vishwajabakathatla@users.noreply.github.com>
2022-07-07 15:50:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+function preload() {
+    clown_nose=loadImage('https://i.postimg.cc/PxFvYgkv/l1.png')
+}
+noseX=0;
+noseY=0;
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modelLoaded() {
+    console.log('poseNet Is Initialized')
+}
+
+function draw() {
+    image(video, 0, 0, 300, 300);
+    fill(255,0,0);
+    stroke(255,0,0);
+    image(clown_nose,noseX-20,noseY+10,45,45);
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+
+
+}
+
+function gotPoses(results) {
+    {
+
+        if (results.length > 0);
+    
+    console.log(results);
+    noseX=results[0].pose.nose.x;
+    noseY=results[0].pose.nose.y;
+    console.log("nose x = " + results[0].pose.nose.x);
+    console.log("nose Y = " + results[0].pose.nose.y); 
+    }
+}
